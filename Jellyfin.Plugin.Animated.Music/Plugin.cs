53895dd378af986cb1aaf280b76fe9f17047aa2a
Elias <elias@deuss.dev>
2025-07-13 13:21:20 +0200

    fix: add empty config

--- Jellyfin.Plugin.Animated.Music/Plugin.cs
+++ Jellyfin.Plugin.Animated.Music/Plugin.cs
@@ -10,7 +10,7 @@ namespace Jellyfin.Plugin.Animated.Music
     /// <summary>
     /// Main plugin class for Jellyfin.Plugin.Animated.Music.
     /// </summary>
-    public class Plugin : BasePlugin
+    public class Plugin : BasePlugin<PluginConfiguration>
     {
         /// <summary>
         /// Gets the plugin instance.
@@ -21,8 +21,9 @@ namespace Jellyfin.Plugin.Animated.Music
         /// Initializes a new instance of the <see cref="Plugin"/> class.
         /// </summary>
         /// <param name="applicationPaths">Instance of the <see cref="IApplicationPaths"/> interface.</param>
-        public Plugin(IApplicationPaths applicationPaths)
-            : base()
+        /// <param name="xmlSerializer">Instance of the <see cref="IXmlSerializer"/> interface.</param>
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
         {
             Instance = this;
         }
@@ -35,5 +36,13 @@ namespace Jellyfin.Plugin.Animated.Music
 
         /// <inheritdoc />
         public override string Description => "Adds animated cover and vertical video background support for music albums";
+    }
+
+    /// <summary>
+    /// Plugin configuration.
+    /// </summary>
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+        // Empty configuration for now
     }
 } 
